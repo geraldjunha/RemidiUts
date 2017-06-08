a5874d90723fdbd24d772c606d7531324072bede
M.Gerald Junhakristama <geraldjunha@gmail.com>
2017-06-08 07:16:18 +0700

    Finish

--- app/src/main/java/xir1n17/remidiuts/telkomschid/remidiuts/MainActivity.java
+++ app/src/main/java/xir1n17/remidiuts/telkomschid/remidiuts/MainActivity.java
@@ -1,7 +1,9 @@
 package xir1n17.remidiuts.telkomschid.remidiuts;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.swipe).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SwipeActivity.class));
+            }
+        });
+
+        findViewById(R.id.tab).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TabActivity.class));
+            }
+        });
+
+        findViewById(R.id.spin).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SpinActivity.class));
+            }
+        });
+
+
+        findViewById(R.id.nav).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, NavActivity.class));
+            }
+        });
+
+
     }
 }
